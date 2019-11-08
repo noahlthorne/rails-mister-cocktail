aa5d1927e058bab23ddbc8c9fd1be0e70ad0b28a
Noah Thorne <noahlthorne@gmail.com>
2019-11-08 17:26:10 +0900

    finished

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,13 +1,15 @@
 import swal from 'sweetalert';
 
-const initSweetalert = (selector, options = {}) => {
+const initSweetalert = (selector, options = {}, callback = (id) => {
+  fetch(`/cocktails/${id}`, { method: "delete"})
+}) => {
   const swalButton = document.querySelector(selector);
   if (swalButton) { // protect other pages
     swalButton.addEventListener('click', (event) => {
-      console.log(event)
-      swal(options)
+      const cocktailId = event.target.dataset.cocktailId
+      swal(options).then(callback(cocktailId));
     });
   }
 };
-
 export { initSweetalert };
+
