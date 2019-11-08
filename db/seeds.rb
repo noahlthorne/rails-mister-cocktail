d650be69b89f778e9f10de8bc66bddec69a107eb
Noah Thorne <noahlthorne@gmail.com>
2019-11-08 11:32:27 +0900

    added a path to root

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'json'
 puts 'Creating new ingredients...'
 
+Ingredient.delete_all
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 url_serialized = URI.parse(url).read
 ingredients = JSON.parse(url_serialized)
